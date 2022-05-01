a13b5133ef8e1ec1f377528668ba6c8f78a49c7b
Phoebe Chang <phoebe03111@gmail.com>
2022-05-01 16:55:38 -0700

    Create navigation and hamburger menu in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Header.scss";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => setShowNav(!showNav);
+
+  console.log(showNav);
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="logo" />
-
-      <nav>
-        <ul className="nav__list">
-          <li className="nav__list-item">About</li>
-          <li className="nav__list-item">Projects</li>
-          <li className="nav__list-item">Contact</li>
+      <a href="/">
+        <img className="header__logo" src={logo} alt="logo" />
+      </a>
+      
+      <nav className="nav">
+        <ul style={{ display: showNav ? "flex" : null }} className="nav__list">
+          <li className="nav__list-item">
+            <a href="#about" onClick={toggleNav}>
+              About
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="#projects" onClick={toggleNav}>
+              Projects
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="#contact" onClick={toggleNav}>
+              Contact
+            </a>
+          </li>
         </ul>
+
+        <button className="nav__button" onClick={toggleNav}>
+          {showNav ? <FaTimes /> : <FaBars />}
+        </button>
       </nav>
     </header>
   );

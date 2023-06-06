2a0f5c1ddee343395f8d5b6e0a01bae9199f75e9
Phoebe Chang <phoebe03111@gmail.com>
2023-06-06 15:21:54 -0700

    Update summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,13 +8,25 @@ const About = () => {
       <div className="about__intro">
         <h1>Hi there! I am Phoebe.</h1>
         <h3>A full stack web developer based in Vancouver, Canada.</h3>
-        <p>
+        <p style={{ textAlign: "justify" }}>
           <span className="about__highlight">A little bit about myself - </span>
-          I switched my career and relocated to a new country during pandemic.
-          After several months of self-learning, I joined BrainStation Web
-          Development bootcamp and became a teaching assistant after graduation.
-          I am passionate about learning new technologies to create projects, as
-          well as building user-friendly and engaging websites and applications.
+          I started my journey by transitioning careers during the pandemic and
+          relocating to a new country. I joined the BrainStation Web Development
+          bootcamp, where I not only completed the program but also became a
+          teaching assistant, helping aspiring developers achieve their goals.
+          Currently I am working as a Full Stack Developer at{" "}
+          <a
+            href="https://www.ficklmarketing.com/"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "underline" }}
+          >
+            Fickl
+          </a>
+          , where I contributed to the development of user-friendly and dynamic
+          web application. I am passionate about staying up-to-date with the
+          latest technologies and continuously enhancing my skills to deliver
+          exceptional results.
         </p>
       </div>
 
